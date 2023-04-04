4750a236437302fb42fbcc0ab179f0e50c33b085
Sebastian <shuertas981@gmail.com>
2023-04-03 21:37:28 -0500

    Refactorizando responsabilidades

--- services/client-service.js
+++ services/client-service.js
@@ -1,50 +1,5 @@
 
 
-const crearNuevaLinea= (nombre, email)=>{
-
-    // comillas para poder usar html en js -> ``
-
-    //generar una etiqueta tr
-    const linea = document.createElement("tr");
-    const contenido =  `
-   <td class="td" data-td>
-   ${nombre}</td>
-   <td>${email}</td>
-   <td>
-     <ul class="table__button-control">
-       <li>
-         <a
-           href="../screens/editar_cliente.html"
-           class="simple-button simple-button--edit"
-           >Editar</a
-         >
-       </li>
-       <li>
-         <button
-           class="simple-button simple-button--delete"
-           type="button"
-         >
-           Eliminar
-         </button>
-       </li>
-     </ul>
-   </td>
-`;
-
-    linea.innerHTML = contenido;
-    return linea; // ya deberia ser el codigo html rellenado con el const contenido
-};
-//nueva comunicacion entre en front end y backend
-
-
-
-
-//XML HTTP REQUEST => 
-
-
-//recorrerá todo el arbol del DOM y obtener el elemento data-table
-const table =document.querySelector('[data-table]');
-
 const http = new XMLHttpRequest();
 
 //Abrir http, open -> metodo que recibe 2 parametros
@@ -60,24 +15,11 @@ Delete(borrar) ->DELETE
 //Fetch API -> funcion que viene por defecto
 
 
-const listaClientes = () =>{
-   return fetch("http://localhost:3000/perfil").then( respuesta => respuesta.json());//A mi respuesta darle un formato json
-   };
-//then se ejecuta en caso tenga la informacion, en este caso la data
-listaClientes()
-.then((data) =>{
-    data.forEach(perfil => {
-        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-         table.appendChild(nuevaLinea);
-     });
-}).catch((error) => alert("Ocurrió un error"));
+const listaClientes = () =>
+   fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());//A mi respuesta darle un formato json
+  
 
-//una vez que cargues o termines de recibir esta respuesta va a ejecutarse esta consola
-
-
-
-
+export const clientServices = {
+  listaClientes
+};
 //necesitamos generar la estructura de lista en nuestro archivo js
-
-
-//console.log(http);
